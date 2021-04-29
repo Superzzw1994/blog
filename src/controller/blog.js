a0b15e5263d7a1e8bb6cb2f97d9337347930a74d
邪恶天才 <shzx1994529@gmail.com>
2021-04-29 23:01:16 +0800

    初始化项目

---
+++ src/controller/blog.js
@@ -0,0 +1,15 @@
+const getList = (author, keyword) => {
+  return [
+    {
+      id: 1,
+      title: 'title',
+      content: 'content',
+      author: 'zzw',
+      createTime: '456'
+    }
+  ]
+}
+
+module.exports = {
+  getList
+}
