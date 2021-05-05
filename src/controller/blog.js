6f9c6ac720d52f29ac349e6c6dd007f2d7d40e9d
zhiwei.zheng <zhiwei.zheng@cloudwise.com>
2021-05-05 18:03:08 +0800

    添加 controller

--- src/controller/blog.js
+++ src/controller/blog.js
@@ -1,15 +1,22 @@
 const getList = (author, keyword) => {
-  return [
-    {
-      id: 1,
-      title: 'title',
-      content: 'content',
-      author: 'zzw',
-      createTime: '456'
-    }
-  ]
+    return [
+        {
+            id: 1,
+            title: 'title',
+            content: 'content',
+            author: 'zzw',
+            createTime: '456'
+        }
+    ]
 }
 
+const getDetail = (id = 0) => {
+    return {
+        id,
+        name: 'zzw'
+    }
+}
 module.exports = {
-  getList
+    getList,
+    getDetail
 }
--- src/router/blog.js
+++ src/router/blog.js
@@ -1,37 +1,37 @@
 const {getPropertyFromRequest} = require('../utils')
 const {SuccessModel, ErrorModel} = require('../model/restModal')
-const {getList} = require('../controller/blog')
+const {getList, getDetail} = require('../controller/blog')
 const handleBlogRouter = (req, res) => {
-  const {method, path} = getPropertyFromRequest(req)
-  if (method === 'GET' && path === '/api/blog/list') {
-    const {author, keyword} = req.query
-    const data = getList(author, keyword)
-    return new SuccessModel(data)
-  }
-
-  if (method === 'GET' && path === '/api/blog/detail') {
-    return {
-      msg: '这是获取博客详情的接口'
+    const {method, path} = getPropertyFromRequest(req)
+    if (method === 'GET' && path === '/api/blog/list') {
+        const {author, keyword} = req.query
+        const data = getList(author, keyword)
+        return new SuccessModel(data)
     }
-  }
 
-  if (method === 'POST' && path === '/api/blog/add') {
-    return {
-      msg: '这是新建博客的接口'
+    if (method === 'GET' && path === '/api/blog/detail') {
+        const {id = 0} = req.query
+        const data = getDetail(id)
+        return new SuccessModel(data)
     }
-  }
 
-  if (method === 'POST' && path === '/api/blog/update') {
-    return {
-      msg: '这是更新博客的接口'
+    if (method === 'POST' && path === '/api/blog/add') {
+        return {
+            msg: '这是新建博客的接口'
+        }
     }
-  }
 
-  if (method === 'POST' && path === '/api/blog/delete') {
-    return {
-      msg: '这是删除博客的接口'
+    if (method === 'POST' && path === '/api/blog/update') {
+        return {
+            msg: '这是更新博客的接口'
+        }
     }
-  }
+
+    if (method === 'POST' && path === '/api/blog/delete') {
+        return {
+            msg: '这是删除博客的接口'
+        }
+    }
 }
 
 module.exports = handleBlogRouter
