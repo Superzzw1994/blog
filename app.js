fa5bd6f3c569e3ead8dc0661595126ad99a71d8d
邪恶天才 <shzx1994529@gmail.com>
2021-05-12 12:37:38 +0800

    初始化express

--- app.js
+++ app.js
@@ -1,85 +1,41 @@
-const queryString = require('querystring')
-const handleUserRouter = require('./src/router/user')
-const handleBlogRouter = require('./src/router/blog')
-const {get, set} = require('./src/db/redis')
-const {access} = require('./src/utils/log')
-const getPostData = (req) => new Promise((resolve, reject) => {
-  if (req.method !== 'POST') {
-    return resolve({})
-  }
-  if (req.headers['content-type'] !== 'application/json') {
-    return resolve({})
-  }
-  let postData = ''
-  req.on('data', chunk => postData += chunk.toString())
-  req.on('end', () => {
-    if (!postData) {
-      return resolve({})
-    }
-    resolve(JSON.parse(postData))
-  })
-})
-const getCookieExpires = () => {
-  const d = new Date()
-  d.setTime(d.getTime() + (24 * 60 * 60 * 1000))
-  return d.toGMTString()
-}
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const serverHandle = (req, res) => {
-  access(`${req.method} -- ${req.url} -- ${req.headers['user-agent']} -- ${Date.now()}`)
-  res.setHeader('Content-type', 'application/json')
-  req.query = queryString.parse(req.url.split('?')[1])
-  req.cookie = {}
-  const cookieStr = req.headers.cookie || ''
-  cookieStr.split(';').forEach(item => {
-    if (!item) {
-      return
-    }
-    const arr = item.split('=')
-    req.cookie[arr[0].trim()] = arr[1].trim()
-  })
-  let needSetCookie = false
-  let userId = req.cookie.userId
-  if (!userId) {
-    needSetCookie = true
-    userId = `${Date.now()}_${Math.random()}`
-    req.sessionId = userId
-    set(userId, {})
-  }
-  get(userId).then(sessionData => {
-    if (sessionData === null) {
-      set(userId, {})
-      req.session = {}
-    } else {
-      req.session = sessionData
-    }
-    return getPostData(req)
-  }).then(postData => {
-    req.body = postData
-    const blogData = handleBlogRouter(req, res)
-    const userData = handleUserRouter(req, res)
-    if (blogData) {
-      return blogData.then(data => {
-        needSetCookie && res.setHeader('Set-Cookie', `userId=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
-        needSetCookie = false
-        return res.end(JSON.stringify(data))
-      })
-    }
-    if (userData) {
-      return userData.then(data => {
-        needSetCookie && res.setHeader('Set-Cookie', `userId=${userId}; path=/; httpOnly; expires=${getCookieExpires()}`)
-        needSetCookie = false
-        return res.end(JSON.stringify(data))
-      })
-    }
-    res.writeHead(404, {
-      "Content-type": "text/plain"
-    })
-    res.write("404 not found")
-    res.end()
-  })
-}
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-module.exports = {
-  serverHandle
-}
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
